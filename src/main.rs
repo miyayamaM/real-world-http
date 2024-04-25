9b57fc727f8276773cfbfb3c6daaf2332cc3d32e
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-04-25 21:19:24 +0900

    feat: output request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use axum::extract::Request;
+use axum::response::Html;
 use axum::routing::get;
 use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/greeting", get(greeting));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:18888")
         .await
@@ -11,6 +13,7 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn greeting(request: Request) -> Html<&'static str> {
+    println!("{:#?}", request);
+    Html("<html><body>Hello, World!</body></html>")
 }
