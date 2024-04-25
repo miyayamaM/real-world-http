1c1485ff8b24abb3f8b0ec24f932aabd50b063e3
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-04-25 21:13:05 +0900

    feat: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:18888")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
