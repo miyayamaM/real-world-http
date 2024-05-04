c98add38eeeda5e692ef06296af777248be9a007
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-05-04 10:32:08 +0900

    feat: send query parameter

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,11 @@
 use reqwest::Error;
 
 fn main() -> Result<(), Error> {
-    let response = reqwest::blocking::get("http://localhost:18888/greeting")?;
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .get("http://localhost:18888/greeting")
+        .query(&[("name", "hello world")])
+        .send()?;
     println!("status code: {}", response.status().as_u16());
     if let Some(message) = response.status().canonical_reason() {
         println!("status: {}", message);
