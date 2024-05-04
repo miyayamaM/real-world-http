aa37e3ce00e2e1c0c787519871daa7d5d5c95598
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-05-04 10:12:21 +0900

    feat: output request info

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,14 @@
 use reqwest::Error;
 
 fn main() -> Result<(), Error> {
-    let body = reqwest::blocking::get("http://localhost:18888/greeting")?.text()?;
-    println!("{}", body);
+    let response = reqwest::blocking::get("http://localhost:18888/greeting")?;
+    println!("status code: {}", response.status().as_u16());
+    if let Some(message) = response.status().canonical_reason() {
+        println!("status: {}", message);
+    }
+    for (header_key, header_value) in response.headers().iter() {
+        println!("{:?}: {:?}", header_key, header_value)
+    }
+    println!("response: {}", response.text()?);
     Ok(())
 }
